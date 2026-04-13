34fbb6fb13cb9193068bd74131cdc08282375ccc
Hemorxt <jcontreras.exp@gmail.com>
2026-04-13 09:45:59 -0400

    creando variables

--- prueba.py
+++ prueba.py
@@ -1,1 +1,19 @@
-print("")
+print("Hola Azeroth")
+
+# Crendo Variables
+
+titulo="Clima de hoy" #String
+diaDelMes=13          #Int
+mes=4                 #Int
+
+temperatura=22.3      #Float
+llueve=False          #boolean
+
+print(titulo)
+print("Temperatura Actual:", temperatura, "grados")
+print(diaDelMes, "-",mes)
+
+if llueve:
+    print("Tiene que llevar paraguas")
+else:
+    print("puede llevar polera sin mangas")
